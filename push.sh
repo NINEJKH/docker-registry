70c1ca20985a96e23691e51a533226d5734b7a2e
Gunter Grodotzki <gunter@grodotzki.co.za>
2018-05-18 16:09:26 +0200

    enable travis + push to docker-hub

--- push.sh
+++ push.sh
@@ -2,5 +2,15 @@
 
 set -e
 
+if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
+  TAG="latest"
+else
+  TAG="${TRAVIS_BRANCH}"
+fi
+
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push 9jkh/registry
+
+docker tag "${DOCKER_REPO}:${COMMIT}" "${DOCKER_REPO}:${TAG}"
+docker tag "${DOCKER_REPO}:${COMMIT}" "${DOCKER_REPO}:travis-${TRAVIS_BUILD_NUMBER}"
+
+docker push "${DOCKER_REPO}"
